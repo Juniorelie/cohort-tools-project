1170895b3b950b6dca32f74fbaf8e1335ca4066d
Mark Landers <alexandre@dashlane.com>
2024-06-25 17:11:27 +0200

    add students schema

---
+++ server/models/students.models.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure of the documents
+const studentSchema = new Schema({
+  firstName: { type: string, required: true },
+  lastName: { type: string, required: true },
+  email: { type: string, required: true, unique: true },
+  phone: { type: string, required: true },
+  linkedinUrl: { type: string, default: "" },
+  languages: {
+    type: String,
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: string, default: "" },
+  image: { type: string, default: "https://i.imgur.com/r8bo8u7.png" },
+  cohort: ObjectId,
+  projects: Array,
+});
+
+// EXPORT THE MODEL
+module.exports = studentSchema;

5add4fbdcd83a6af686845d58ae894e011dcdafb
Mark Landers <alexandre@dashlane.com>
2024-06-26 17:55:28 +0200

    Add students API routes

--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -13,4 +13,115 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    linkedinUrl,
+    languages,
+    program,
+    background,
+    image,
+    cohort,
+    projects,
+  } = req.body;
+  const studentToCreate = {
+    firstName,
+    lastName,
+    email,
+    phone,
+    linkedinUrl,
+    languages,
+    program,
+    background,
+    image,
+    cohort,
+    projects,
+  };
+
+  const createdStudent = await Student.create(studentToCreate);
+
+  res.status(201).json(createdStudent);
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:studentId", async (req, res, next) => {
+  const { studentId } = req.params;
+  const oneStudent = await Student.findOne({ _id: studentId });
+  res.json(oneStudent);
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/cohort/:cohortId", async (req, res, next) => {
+  const { cohortId } = req.params;
+  const students = await Student.find({ cohort: cohortId });
+  res.json(students);
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/:studentId", async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      linkedinUrl,
+      languages,
+      program,
+      background,
+      image,
+      cohort,
+      projects,
+    } = req.body;
+    const { studentId } = req.params;
+    const studentToUpdate = {
+      firstName,
+      lastName,
+      email,
+      phone,
+      linkedinUrl,
+      languages,
+      program,
+      background,
+      image,
+      cohort,
+      projects,
+    };
+
+    // Find and update
+    const updatedStudent = await Student.findOneAndUpdate(
+      { _id: studentId },
+      studentToUpdate,
+      {
+        new: true,
+      }
+    );
+    res.status(202).json(updatedStudent);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/:studentId", async (req, res, next) => {
+  try {
+    const { studentId } = req.params;
+    await Student.findOneAndDelete({ _id: studentId });
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;

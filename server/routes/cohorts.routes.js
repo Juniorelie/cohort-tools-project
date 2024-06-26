9d5d01b421b8ad952ac17b24e7af2f0b983b3235
Mark Landers <alexandre@dashlane.com>
2024-06-26 16:18:27 +0200

    Fix files path

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -1,15 +1,15 @@
-const router = require("express").Router()
-const Cohort = require("./../model/Cohort.model")
+const router = require("express").Router();
+const Cohort = require("./../models/cohorts.model");
 //! Those routes are prefixed with /api/cohorts
 
 router.get("/", async (req, res, next) => {
-	// TODO
-	try {
-		const cohorts = await Cohort.find()
-		res.json(cohorts)
-	} catch (error) {
-		console.log(error)
-	}
-})
+  // TODO
+  try {
+    const cohorts = await Cohort.find();
+    res.json(cohorts);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -1,16 +1,16 @@
-const router = require("express").Router()
-const Student = require("./../model/Student.model")
+const router = require("express").Router();
+const Student = require("./../models/students.models");
 
 //! Those routes are prefixed with /api/students
 
 router.get("/", async (req, res, next) => {
-	// TODO
-	try {
-		const students = await Student.find()
-		res.json(students)
-	} catch (error) {
-		console.log(error)
-	}
-})
+  // TODO
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;

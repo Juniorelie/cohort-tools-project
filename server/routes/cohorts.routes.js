e7a9534c5c7702e99c88e7d079734644afc26ffc
Kaiser_Elie <mbakopjuniorelie@gmail.com>
2024-06-26 17:56:18 +0200

    created guet, put and post for cohort

--- server/routes/cohorts.routes.js
+++ server/routes/cohorts.routes.js
@@ -12,4 +12,69 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// Retrieving cohort by ID
+router.get("/:cohortId", (req, res, next) => {
+  Cohort.findById(req.params.cohortId)
+    .then((cohort) => {
+      console.log("Cohort found: ", cohort);
+      res.status(200).json(cohort);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohort", error);
+    });
+});
+
+// Creating a new cohort
+router.post("/", (req, res, next) => {
+  Cohort.create(req.body)
+    .then((newCohort) => {
+      console.log("New cohort created: ", newCohort);
+      res.status(201).json(newCohort);
+    })
+    .catch((error) => {
+      console.error("Error while creating cohort ->", error);
+    });
+});
+
+// Updating cohort by ID
+router.put("/:cohortId", (req, res, next) => {
+  Cohort.findByIdAndUpdate(
+    req.params.cohortId,
+    ({
+      cohortSlug,
+      cohortName,
+      program,
+      format,
+      campus,
+      startDate,
+      endDate,
+      inProgress,
+      programManager,
+      leadTeacher,
+      totalHours,
+    } = req.body),
+    { new: true }
+  )
+    .then((updatedCohort) => {
+      console.log("Cohort updated: ", updatedCohort);
+      res.status(202).json(updatedCohort);
+    })
+    .catch((error) => {
+      console.error("Error while updating cohort: ", error);
+      res.status(500);
+    });
+});
+
+// Deleting cohort by ID
+router.delete("/:cohortId", (req, res, next) => {
+  Cohort.findByIdAndDelete(req.params.cohortId)
+    .then((deletedCohort) => {
+      console.log("Cohort deleted: ", deletedCohort);
+      res.status(204).json(`Cohort id ${req.params.cohortId} was deleted`);
+    })
+    .catch((error) => {
+      console.error("Error while deleting cohort: ", error);
+    });
+});
+
 module.exports = router;

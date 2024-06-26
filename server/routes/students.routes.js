1e1dac56094b1b85bb86494b098bb2409eb34db7
Mark Landers <alexandre@dashlane.com>
2024-06-26 16:29:26 +0200

    add students routes

--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -13,4 +13,20 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/students", (req, res) => {
+  Student.find({})
+
+    .then((students) => {
+      console.log("Retrieved students ->", students);
+
+      res.json(students);
+    })
+
+    .catch((error) => {
+      console.error("Error while retrieving students ->", error);
+
+      res.status(500).json({ error: "Failed to retrieve students" });
+    });
+});
+
 module.exports = router;

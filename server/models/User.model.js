ef7ed41567b903adff6d77eaa987318f8fcac237
Mark Landers <alexandre@dashlane.com>
2024-06-28 12:34:30 +0200

    remove role

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -18,11 +18,6 @@ const userSchema = new Schema(
       required: true,
       select: false,
     },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
   },
   { timestamps: true }
 );

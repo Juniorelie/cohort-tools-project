243c2f614b745d04b1cc83056fc5f7342c7c8d05
Mark Landers <alexandre@dashlane.com>
2024-06-28 12:33:33 +0200

    add user model

---
+++ server/models/User.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      maxLength: 50,
+      minLength: 2,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+module.exports = User;
